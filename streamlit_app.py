52256478170128be804f850fa17f9c1454291f27
sharad-jn <147039957+sharad-jn@users.noreply.github.com>
2023-10-05 16:36:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,6 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Avacado Toast')
 
 streamlit.header('Biuld Your Own Fruit Smoothie')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
