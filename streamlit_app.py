8877c1ce9823af62b1fd23fbfe582c246e102239
sharad-jn <147039957+sharad-jn@users.noreply.github.com>
2023-10-05 15:38:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('May Parents New Healthy Dinner')
+streamlit.title('May Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
